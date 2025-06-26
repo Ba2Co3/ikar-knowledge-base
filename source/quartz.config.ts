9055e8e3283ccf478c2173c14fec61a0e387d9a7
Ba2Co3 <sgamak97@gmail.com>
2025-06-26 19:13:10 +0200

    vault backup: 2025-06-26 19:13:10

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Икар",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
